e19b19044e4d3968a39d106691f897f655484b37
nawab zada <zada86829@gmail.com>
2025-11-01 01:40:07 +0500

    updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,7 +27,7 @@ const Hero = () => {
       {/* Background image with overlay */}
       <div className="position-absolute top-0 start-0 w-100 h-100 z-0">
         <Image
-          src="/helixyneee.jpg"
+          src="/helixynehero.jpg"
           alt="Hero Background"
           fill
           style={{ objectFit: 'cover' }}
@@ -44,9 +44,9 @@ const Hero = () => {
 
       {/* Foreground caption content */}
       <div className="container caption position-relative z-1">
-        <div className="row" style={{ height: '85vh' }}>
+        <div className="row">
           <div
-            className="col-lg-8 p-5 text-center text-sm-start d-flex flex-column justify-content-center rounded"
+            className="col-lg-8 p-3 p-md-5 text-center d-flex flex-column justify-content-center rounded"
             data-cues="slideInDown"
             data-group="page-title"
             data-interval="-200"
@@ -56,8 +56,8 @@ const Hero = () => {
               Introducing the Helixyne Indenter
             </h2>
 
-            <p className="lead fs-23 lh-sm mb-7 text-white animate__animated animate__slideInRight animate__delay-2s">
-              Experience precision and innovation with the Helixyne Indenter, a revolutionary device designed to elevate diagnostic accuracy and treatment efficacy. Engineered with cutting-edge technology, it provides unparalleled insights for medical professionals, ensuring superior patient outcomes. Discover the future of medical diagnostics.
+            <p className="lead fs-23 lh-sm mb-7 text-white text-center animate__animated animate__slideInRight animate__delay-2s">
+            Helixyne Indenter is a trusted global pharmaceutical sourcing partner, connecting healthcare providers with top-quality medicines and medical supplies. We ensure reliability, transparency, and efficiency in every transaction. Empowering better health through seamless global trade and trusted partnerships.
             </p>
 
             <div className="animate__animated animate__slideInUp animate__delay-3s">
